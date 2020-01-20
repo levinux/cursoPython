55bf7de845160e493afde8162864a09950c3e160
Levi Sandoval <levisan@gmail.com>
2020-01-20 09:42:53 -0700

    nuevo archivo sobre listas y tuplas

---
+++ dia1/scripts/dia1-02.py
@@ -0,0 +1,13 @@
+# Declaracion de listas
+
+milista = [ "mangos", "platanos", "fresas" ]
+mi2lista = list([i for i in range(10)])
+
+print(milista)
+print(mi2lista)
+
+# Operaciones de suma, multiplicacion
+unos = [1] * 3
+muchos = mi2lista + unos
+print(unos)
+print(muchos)
